c18d702562f2d4518f5f820a19af16ed584958fb
harabla <blankenstein.harri@gmail.com>
2022-05-01 15:07:26 +0300

    - Max putts sends data to firebase.

--- app/src/main/java/com/example/the360/maxPuttsSettings.java
+++ app/src/main/java/com/example/the360/maxPuttsSettings.java
@@ -1,17 +1,29 @@
 package com.example.the360;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class maxPuttsSettings extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,8 @@ public class maxPuttsSettings extends AppCompatActivity {
         Button mpNewRound = findViewById(R.id.mpNewRound);
         Button mpSaveExit = findViewById(R.id.mpFinishExit);
         Button mpExit = findViewById(R.id.mpExit);
+
+        EditText score = findViewById(R.id.maxPuttsmade);
 
         RadioGroup radioGroupLocation = (RadioGroup) findViewById(R.id.puttLocation);
         RadioGroup radioGroupWindSelectWindSpeed = (RadioGroup) findViewById(R.id.windSpeed);
@@ -39,6 +53,13 @@ public class maxPuttsSettings extends AppCompatActivity {
 
         //lets give default checked states for radio buttons
 
+        /// Time to make life difficult -- firebase
+        databaseReference = FirebaseDatabase.getInstance("https://the360-70adc-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+
+        //test user
+        String user = "Harri";
+
+
 
 
         //What do those buttons do
@@ -47,9 +68,24 @@ public class maxPuttsSettings extends AppCompatActivity {
         mpNewRound.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+                String stscore = score.getText().toString();
+                int finalscore = Integer.parseInt(stscore);
+
+                Long time = System.currentTimeMillis()/1000;
+                String ts = time.toString();
+
+                databaseReference.child("Max Putts").child(user).child(ts).setValue(String.valueOf(finalscore));
+
+
                 Toast.makeText(getApplicationContext(),"This should save data to firebase & clear putt count - other settings to stay the same",Toast.LENGTH_SHORT).show();
+
             }
+
         });
+
+
 
 
         //MP save exit = should save data to firebase and exit to the front page
@@ -57,6 +93,29 @@ public class maxPuttsSettings extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"This should save data to firebase and exit to the front page",Toast.LENGTH_SHORT).show();
+
+                databaseReference.setValue("160").addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+                        Log.i("succeeding", "success");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+                        Log.i("failing", "failed");
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "wtf", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                Intent intent = new Intent(maxPuttsSettings.this,MainActivity.class);
+                startActivity(intent);
+
             }
         });
 
@@ -65,6 +124,9 @@ public class maxPuttsSettings extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"This should simply exit to the front page",Toast.LENGTH_SHORT).show();
+
+
+
                 Intent intent = new Intent(maxPuttsSettings.this,MainActivity.class);
                 startActivity(intent);
             }
@@ -122,6 +184,7 @@ public class maxPuttsSettings extends AppCompatActivity {
                 }
             }
         });
+
 
 
     }
