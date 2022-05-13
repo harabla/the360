e39be013a1ecaeafb7c957c663c9263011e19d57
Harri <blankenstein.harri@gmail.com>
2022-05-13 15:37:23 +0300

    - Created nav and transitions between different pages.

--- app/src/main/java/com/example/the360/AnalysisActivity.java
+++ app/src/main/java/com/example/the360/AnalysisActivity.java
@@ -2,13 +2,11 @@ package com.example.the360;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,17 +16,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
 
 import java.util.Calendar;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class AnalysisActivity extends AppCompatActivity {
 
-    private Button maxPuttsSelect;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
@@ -38,9 +32,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_analysis);
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         getSupportActionBar().hide();
 
         this.getWindow().getDecorView().setSystemUiVisibility(
@@ -51,9 +44,9 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-
         firebaseDatabase = FirebaseDatabase.getInstance("https://the360-70adc-default-rtdb.europe-west1.firebasedatabase.app/");
         databaseReference = firebaseDatabase.getReference();
+
         getUserProfile();
 
 
@@ -64,30 +57,6 @@ public class MainActivity extends AppCompatActivity {
             fillInHeader(uid);
         }
 
-
-        //list buttons
-
-
-
-
-        Button login, register, logout;
-        login = findViewById(R.id.Login);
-        register = findViewById(R.id.Register);
-        logout = findViewById(R.id.logout);
-
-        //Putting
-        Button maxPuttsSelect = findViewById(R.id.maxputts);
-        Button jylySelect = findViewById(R.id.jyly);
-
-        //Driving
-        Button accuracySelect = findViewById(R.id.accuracy);
-        Button distanceSelect = findViewById(R.id.distance);
-        Button speedSelect = findViewById(R.id.speed);
-
-        //Round stats
-        Button itbSelect = findViewById(R.id.inthebag);
-        Button roundsummarySelect = findViewById(R.id.roundsum);
-
         // Nav buttons
 
         Button navMain = findViewById(R.id.navMain);
@@ -95,94 +64,14 @@ public class MainActivity extends AppCompatActivity {
         Button navDriving = findViewById(R.id.navDriving);
         Button navAnalysis = findViewById(R.id.navAnalysis);
 
-        //What happens on button clicks
-
-        // Max putts selected
-        maxPuttsSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,maxPuttsSettings.class);
-                startActivity(intent);
-            }
-        });
-
-        jylySelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,JylySettings.class);
-                startActivity(intent);
-            }
-        });
-
-        accuracySelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),R.string.midterm,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        distanceSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),R.string.midterm,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        speedSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),R.string.midterm,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        itbSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),R.string.longterm,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        roundsummarySelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),R.string.longterm,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),R.string.shortterm,Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(MainActivity.this,login.class);
-                startActivity(intent);
-            }
-        });
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),R.string.shortterm,Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signOut();
-
-            }
-        });
-
         // navigation buttons
 
         navMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent intent = new Intent(AnalysisActivity.this, MainActivity.class);
+                startActivity(intent);
 
             }
         });
@@ -191,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(MainActivity.this, PuttingActivity.class);
+                Intent intent = new Intent(AnalysisActivity.this, PuttingActivity.class);
                 startActivity(intent);
 
             }
@@ -201,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(MainActivity.this, DrivingActivity.class);
+                Intent intent = new Intent(AnalysisActivity.this, DrivingActivity.class);
                 startActivity(intent);
 
             }
@@ -211,55 +100,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(MainActivity.this, AnalysisActivity.class);
-                startActivity(intent);
 
             }
         });
-
-
-}
-
-
-
-
-
-    public void getUserProfile() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            String name, email;
-            email = user.getEmail();
-            name = user.getDisplayName();
-
-            //TextView welcomeText = (TextView) findViewById(R.id.mainHeader);
-            //welcomeText.setText("Welcome "+ email + name);
-
-            //String uid = user.getUid();
-
-            Button login = (Button) findViewById(R.id.Login);
-            Button register = (Button) findViewById(R.id.Register);
-            login.setVisibility(View.INVISIBLE);
-            register.setVisibility(View.INVISIBLE);
-
-
-
-        } else {
-            Toast.makeText(this, "Why you not logged in brodda?", Toast.LENGTH_SHORT).show();
-            Button logout = (Button) findViewById(R.id.logout);
-            logout.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    public void signOut() {
-        // [START auth_sign_out]
-        FirebaseAuth.getInstance().signOut();
-        // [END auth_sign_out]
-        Button login = (Button) findViewById(R.id.Login);
-        Button register = (Button) findViewById(R.id.Register);
-        Button logout = (Button) findViewById(R.id.logout);
-        login.setVisibility(View.VISIBLE);
-        register.setVisibility(View.VISIBLE);
-        logout.setVisibility(View.INVISIBLE);
 
     }
 
@@ -347,16 +190,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(AnalysisActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
             }
         });
 
 
-
     }
 
+    public void getUserProfile() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String name, email;
+            email = user.getEmail();
+            name = user.getDisplayName();
 
-
+        } else {
+            Toast.makeText(this, "Why you not logged in brodda?", Toast.LENGTH_SHORT).show();
+            Button logout = (Button) findViewById(R.id.logout);
+            logout.setVisibility(View.INVISIBLE);
+        }
+    }
 }
-
-// this is a change
